8c92a6ff345f964abda0940b4f667184371b0f53
Luke Novak <luken22@optonline.net>
2018-07-14 17:35:29 -0400

    scroll function adjustments

--- nluken.js
+++ nluken.js
@@ -3,19 +3,20 @@ function setScrollListeners() {
   var current = document.getElementById("current-button");
   var faq = document.getElementById("faq-button");
   var title = document.getElementById("big-name");
+  var offset = document.getElementById("top-bar").offsetHeight;
   about.addEventListener("click", function() {
     window.scroll({
-      top: document.getElementById("main-about").offsetTop - 75,
+      top: document.getElementById("main-about").offsetTop - offset,
       behavior: "smooth"});
   });
   current.addEventListener("click", function() {
     window.scroll({
-      top: document.getElementById("current-status").offsetTop - 75,
+      top: document.getElementById("current-status").offsetTop - offset,
       behavior: "smooth"});
   });
   faq.addEventListener("click", function() {
     window.scroll({
-      top: document.getElementById("FAQ").offsetTop - 75,
+      top: document.getElementById("FAQ").offsetTop - offset,
       behavior: "smooth"});
   });
   title.addEventListener("click", function() {
