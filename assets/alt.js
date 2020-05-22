6e272636efcff7b70c78709eebea17a40cc69d4a
Luke Novak <luken22@optonline.net>
2020-05-21 22:43:08 -0400

    added basic animation when question is clicked

--- assets/alt.js
+++ assets/alt.js
@@ -6,11 +6,45 @@ function generateColors() {
     return ["hsl(" + hue + ", " + sat + "%, " + lum + "%)", "hsl(" + hue + ", " + sat + "%, " + (lum - 30) + "%)"];
 }
 
-body = document.getElementsByTagName("body")[0];
-colors = generateColors();
+// works from the question element down, to display the answer and move the title up
+function openAnswer(question, index){
+    // children[0] is the question text, children[1] is the answer
+    children = question.children
+    setTimeout(function(){
+        children[0].classList.remove("straighten-right")
+        question.classList.remove("rotate-slight-left")
+    }, 500);
+    children[0].classList.add("straighten-right")
+    children[1].classList.remove("d-none");
+    question.removeEventListener("click", openHandlers[index]);
+    children[1].addEventListener("click", closeAnswer.bind(null, question));
+}
+
+function closeAnswer(question, index){
+    children = question.children
+    children[1].classList.add("d-none")
+    question.removeEventListener("click", this)
+}
+
+const body = document.getElementsByTagName("body")[0];
+const colors = generateColors();
 body.style.backgroundColor = colors[0];
 body.style.color = colors[1];
-document.getElementsByClassName("outer-border")[0].style.borderColor = colors[1];
+const backButtons = document.getElementsByClassName("back-button");
+for (const button of backButtons) {
+    button.style.backgroundColor = colors[1]
+    button.style.color = colors[0]
+}
+let openHandlers = [];
+i = 0;
+
+// event listeners for the answers
+for (const question of document.getElementsByClassName("question")){
+    openHandlers[i] = (openAnswer.bind(null, question, i))
+    question.addEventListener("click", openHandlers[i]);
+    i++;
+}
+
 
 // event listeners for the letter spinning
 for (const letter of document.getElementsByClassName("circle")){
@@ -26,5 +60,4 @@ for (const letter of document.getElementsByClassName("circle")){
             letter.classList.add("spin-cw")
         })
     }
-
 }
