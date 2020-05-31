4dba70cea4358ec11d94ed638c5b6793b9d1dc37
Luke Novak <luken22@optonline.net>
2020-05-31 06:53:19 -0400

    basic animation smoothed on alt site

--- assets/alt.js
+++ assets/alt.js
@@ -6,24 +6,33 @@ function generateColors() {
     return ["hsl(" + hue + ", " + sat + "%, " + lum + "%)", "hsl(" + hue + ", " + sat + "%, " + (lum - 30) + "%)"];
 }
 
+var closeListener;
+
 // works from the question element down, to display the answer and move the title up
 function openAnswer(question, index){
     // children[0] is the question text, children[1] is the answer
     children = question.children
     setTimeout(function(){
-        children[0].classList.remove("straighten-right")
+        children[0].classList.remove("rt-right")
         question.classList.remove("rotate-slight-left")
+        children[1].classList.remove("d-none");
     }, 500);
-    children[0].classList.add("straighten-right")
-    children[1].classList.remove("d-none");
+    children[0].classList.add("rt-right")
     question.removeEventListener("click", openHandlers[index]);
-    children[1].addEventListener("click", closeAnswer.bind(null, question));
+    closeListener = closeAnswer.bind(null, question, index);
+    children[1].children[1].addEventListener("click", closeAnswer.bind(null, question, index));
 }
 
 function closeAnswer(question, index){
     children = question.children
-    children[1].classList.add("d-none")
-    question.removeEventListener("click", this)
+    setTimeout(function(){
+        children[0].classList.remove("rt-left")
+        question.classList.add("rotate-slight-left")
+        children[1].classList.add("d-none")
+        question.addEventListener("click", openHandlers[index]);
+    }, 500)
+    children[0].classList.add("rt-left");
+    question.children[1].children[1].removeEventListener("click", closeListener)
 }
 
 const body = document.getElementsByTagName("body")[0];
@@ -47,17 +56,32 @@ for (const question of document.getElementsByClassName("question")){
 
 
 // event listeners for the letter spinning
-for (const letter of document.getElementsByClassName("circle")){
+for (const letter of document.getElementsByClassName("circle-right")){
     if (screen.width > 758) {
         letter.addEventListener("mouseover", function(){
-            setTimeout(function(){letter.classList.remove("spin-cw")}, 300);
-            letter.classList.add("spin-cw")
+            setTimeout(function(){letter.classList.remove("spin-cw-right")}, 300);
+            letter.classList.add("spin-cw-right")
         })
     }
     else {
         letter.addEventListener("click", function(){
-            setTimeout(function(){letter.classList.remove("spin-cw")}, 300);
-            letter.classList.add("spin-cw")
+            setTimeout(function(){letter.classList.remove("spin-cw-right")}, 300);
+            letter.classList.add("spin-cw-right")
+        })
+    }
+}
+
+for (const letter of document.getElementsByClassName("circle-left")){
+    if (screen.width > 758) {
+        letter.addEventListener("mouseover", function(){
+            setTimeout(function(){letter.classList.remove("spin-cw-left")}, 300);
+            letter.classList.add("spin-cw-left")
+        })
+    }
+    else {
+        letter.addEventListener("click", function(){
+            setTimeout(function(){letter.classList.remove("spin-cw-left")}, 300);
+            letter.classList.add("spin-cw-left")
         })
     }
 }
