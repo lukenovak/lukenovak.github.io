e73035e2ce3ae92cfea42b4b7e9fe5468609d3cc
Luke Novak <contact@nluken.com>
2026-03-18 16:37:09 -0400

    more tweaks

--- assets/scripts/type.js
+++ assets/scripts/type.js
@@ -3,6 +3,8 @@ let currentPhraseLength = -1;
 let currPhrase =  "";
 let currPhraseIdx = 0;
 let incorrects = [];
+let totalCount = 0;
+let totalWrong = 0;
 let scores = [];
 let startTime = undefined;
 let playedIntro = false;
@@ -10,9 +12,12 @@ let playingIntro = false;
 
 const phrases = [
     "The quick brown fox jumps over the lazy dog.",
-    "I would like to invite you to join my twenty-eigth birthday celebration!",
-    "The party will be on April 18, ten days after my birthday actually occurs,",
-    "starting at 8pm at a bar yet to be confirmed (but probably Antler).",
+    "I can really make you type whatever I want here, huh?",
+    "A quixotic zygote skillfully outmaneuvers its forebearers.",
+    "Anyway, I sent you this webpage to invite you to my birthday party",
+    "\"That sounds fun, when is it?\" I'm glad you asked, completely of your own volition!",
+    "The party will be on April 18, starting at 8pm, at a bar yet to be confirmed",
+    "(the bar will probably be Antler on the Lower East Side).",
     "More details and RSVP can be found at the partiful link below :)"
 ]
 
@@ -52,12 +57,12 @@ const handleNewCharacter = (c) => {
     if (c === currPhrase[currentFocus]) {
         // advance to next element
         currentFocus += 1;
+        totalCount += 1;
 
         if (currentFocus === currentPhraseLength) { // end of phrase
             endTime = Date.now()
-            // calulate speed
-            
-            // Words per minute
+
+            // calulate speed in WPM
             // apparently i'm a wierdo and did Words times 1 / minutes. WHY DID I DO IT THAT WAY
             // stoichiometry still fresh on the mind apparently
             const wpm = (currentPhraseLength / 5) * (60 / ((endTime - startTime) / 1000))
@@ -74,33 +79,35 @@ const handleNewCharacter = (c) => {
             if (currPhraseIdx < phrases.length) {
                 setUpNewPhrase(document.getElementById('question'), currPhraseIdx);
             } else {
-                document.getElementById('question').innerHTML = `thanks for playing ¯\\_(ツ)_/¯`
-                document.getElementById('disclaimer').classList.remove("d-none")
-                document.getElementById('typingContainer').classList.remove('blurred-nonfocus')
-                document.getElementById('beginBox').classList.add('d-none')
-                const scoreboard = document.getElementById('scoreboard')
-                scoreboard.innerHTML = `<div><h4>Your scores:</h4></div>`;
-                for (let i = 0; i < scores.length; i++) {
-                    scoreboard.innerHTML += (`
-                        <div class="row text-center">
-                            <div class="col d-flex">
-                                <h4>Speed:</h4>
-                                <h4 id="speedVal" class="px-2">${scores[i].wpm}</h4>
-                                <h4>WPM</h4>
-                            </div>
-                            <div class="col d-flex">                    
-                                <h4>Accuracy:</h4>
-                                <h4 id="accuracyVal" class="px-2">${scores[i].accuracy}</h4>
-                                <h4>%</h4>
-                            </div>
-                        </div>`)
-                }
+                // game all done in this case
+                document.getElementById('question').innerHTML = `thanks for playing ¯\\_(ツ)_/¯`;
+                document.getElementById('disclaimer').classList.remove("d-none");
+                document.getElementById('typingContainer').classList.remove('blurred-nonfocus');
+                document.getElementById('beginBox').classList.add('d-none');
+                const speedAverage = scores.reduce((acc, n) => acc + parseFloat(n.wpm), 0) / scores.length
+                const overallAccuracy = (1 - (totalWrong / totalCount)) * 100
+                const scoreboard = document.getElementById('scoreboard');
+                scoreboard.innerHTML = `<div><h4>Your Overall Score:</h4></div>`;
+                scoreboard.innerHTML += (`
+                    <div class="row text-center">
+                        <div class="col d-flex">
+                            <h4>Average Speed:</h4>
+                            <h4 id="speedVal" class="px-2">${speedAverage.toFixed(2)}</h4>
+                            <h4>WPM</h4>
+                        </div>
+                        <div class="col d-flex">                    
+                            <h4>Total Accuracy:</h4>
+                            <h4 id="accuracyVal" class="px-2">${overallAccuracy.toFixed(2)}</h4>
+                            <h4>%</h4>
+                        </div>
+                    </div>`)
             }
         } else {
             applyFocus(currentFocus)
         }
     } else if (!incorrects.includes(currentFocus)) {
         incorrects.push(currentFocus);
+        totalWrong += 1;
         document.getElementsByClassName('focused').item(0).classList.add("wrongggg");
     }
 }
